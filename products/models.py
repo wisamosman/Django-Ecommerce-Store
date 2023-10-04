c662eaa47e2fd867092214ed788be64d0f1ca9a6
wisamosman <viesam_1987@hotmail.com>
2023-10-05 00:44:53 +0200

    design products models

--- products/models.py
+++ products/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Product(models.Model):
@@ -6,16 +9,33 @@ class Product(models.Model):
     description = models.TextField(max_length=30000)
     sku = models.IntegerField()
     price = models.FloatField()
+    subtitle = models.TextField(max_length=600)
+    image = models.ImageField(upload_to='products')
+    brand = models.ForeignKey('Brand' , on_delete=models.CASCADE,related_name='product_brand')
 
+    def __str__(self):
+        return f"{self.name} - {self.price}"
 
 class ProductImages(models.Model):
-    pass
+    product = models.ForeignKey(Product,related_name='product_images',on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product_images')
 
 
 class Brand(models.Model):
-    pass
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='brand')
+
+    def __str__(self):
+        return self.name
 
 
 
 class Review(models.Model):
-    pass
+    user = models.ForeignKey(User,related_name='user_review',on_delete=models.SET_NULL,null=True,blank=True)
+    product = models.ForeignKey(Product,related_name='product_review',on_delete=models.SET_NULL,null=True,blank=True)
+    review = models.TextField(max_length=500)
+    rate = models.IntegerField(validators=[MaxValueValidator(5),MinValueValidator(0)])
+    create_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.user} = {self.product}"
