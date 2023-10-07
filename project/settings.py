d51d8bcac5e8a8e8b05a1e5e370960f617ec6689
wisamosman <viesam_1987@hotmail.com>
2023-10-07 23:10:51 +0200

    add django debug toolbar

--- project/settings.py
+++ project/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
     'taggit',
     'django_summernote',
     'settings',
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -73,6 +75,12 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'project.wsgi.application'
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
 
 
 # Database
