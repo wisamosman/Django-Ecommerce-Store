aa9d590b89b803e1f355fec99699899b08b34baf
wisamosman <viesam_1987@hotmail.com>
2023-10-08 10:56:07 +0200

    udate cart context processor

--- orders/cart_context_processor.py
+++ orders/cart_context_processor.py
@@ -5,5 +5,9 @@ from .models import Cart
 
 
 def get_cart_data(request):
-    cart = Cart.objects.get(user=request.user)
-    return {'cart_data':cart}
+    if request.user.is_authenticated:
+        cart = Cart.objects.get_or_create(user=request.user,completed=False)
+        return {'cart_data':cart}
+
+    else:
+        return {}
