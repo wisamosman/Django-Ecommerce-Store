8aded391f05f3e80e050342c125a4c783d27bc8a
wisamosman <viesam_1987@hotmail.com>
2023-10-08 10:48:36 +0200

    add cart_context_processor v1 , add orders listview

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+from .models import Order
+
+
+class OrderList(ListView):
+    model = Order
