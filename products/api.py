4ccd980849c1ddcfb37d311aebe184443481bbcf
wisamosman <viesam_1987@hotmail.com>
2023-10-09 22:14:41 +0200

    add brand list api , fix image in function based view

--- products/api.py
+++ products/api.py
@@ -1,18 +1,25 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import ProductSerializer
-from .models import Product
+from rest_framework import generics
+from .serializers import ProductSerializer , BrandSerializer
+from .models import Product , Brand
 
 
 @api_view(['GET'])
 def product_list_api(request):
     queryset = Product.objects.all()[:10]
-    data = ProductSerializer(queryset,many=True).data
+    data = ProductSerializer(queryset,many=True,context={'request':request}).data
     return Response({'data':data})
 
 
 @api_view(['GET'])
 def product_detail_api(request,product_id):
     queryset = Product.objects.get(id=product_id)
-    data = ProductSerializer(queryset).data
+    data = ProductSerializer(queryset,context={'request':request}).data
     return Response({'data':data})
+
+
+
+class BrandListAPI(generics.ListAPIView):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
