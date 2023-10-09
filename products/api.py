ce612d2dddafd7520b7f141542343ec4da43b242
wisamosman <viesam_1987@hotmail.com>
2023-10-09 22:02:36 +0200

    add api for product detail

--- products/api.py
+++ products/api.py
@@ -9,3 +9,10 @@ def product_list_api(request):
     queryset = Product.objects.all()[:10]
     data = ProductSerializer(queryset,many=True).data
     return Response({'data':data})
+
+
+@api_view(['GET'])
+def product_detail_api(request,product_id):
+    queryset = Product.objects.get(id=product_id)
+    data = ProductSerializer(queryset).data
+    return Response({'data':data})
