046478073e1b4d5daf63b0ec180aaae3b94344d7
wisamosman <viesam_1987@hotmail.com>
2023-10-10 23:16:33 +0200

    add create get or create api

---
+++ orders/api.py
@@ -0,0 +1,21 @@
+from rest_framework import generics
+from rest_framework.response import Response
+from .models import Cart , CartDetail , Order , OrderDetail
+from .serializers import CartDetailSerializer , CartSerializer
+from django.contrib.auth.models import User
+
+
+class CartDetailCreateDeleteAPI(generics.GenericAPIView):
+    def get(self,request,*args, **kwargs):
+        user = User.objects.get(username=self.kwargs['username'])
+        cart , created = Cart.objects.get_or_create(user=user,completed=False)
+        data = CartSerializer(cart).data
+        return Response({'Cart':data , 'Status':200})
+
+
+    def post(self,request,*args, **kwargs):
+        pass
+
+
+    def delete(self,request,*args, **kwargs):
+        pass
---
+++ orders/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Cart , Order , OrderDetail , CartDetail
+
+
+
+class CartDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartDetail
+        fields = '__all__'
+
+
+class CartSerializer(serializers.ModelSerializer):
+    cart_detail = CartDetailSerializer(many=True)
+    class Meta:
+        model = Cart
+        fields = '__all__'
